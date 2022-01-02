1ef5964d79b6c0df5095fc5e4ee4a173694fff6e
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2022-01-02 22:05:29 +0200

    Initial Commit

---
+++ MapAppMVVM/MapAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppMVVMApp.swift
+//  MapAppMVVM
+//
+//  Created by macbook on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
